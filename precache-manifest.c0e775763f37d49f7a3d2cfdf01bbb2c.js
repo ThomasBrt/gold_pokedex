14df62b1916e13c74aaaf87497abdb59db5a0e95
ThomasBrt <thomas.briot@gmail.com>
2020-06-17 20:42:28 +0200

    Updates

---
+++ precache-manifest.c0e775763f37d49f7a3d2cfdf01bbb2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3310274d285620f56f6b8b72343aa6a",
+    "url": "/gold_pokedex/index.html"
+  },
+  {
+    "revision": "2dc9875780053784dd05",
+    "url": "/gold_pokedex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2f51a038cdaf6cc1b1f1",
+    "url": "/gold_pokedex/static/css/main.4d844a19.chunk.css"
+  },
+  {
+    "revision": "2dc9875780053784dd05",
+    "url": "/gold_pokedex/static/js/2.6fb0fff3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gold_pokedex/static/js/2.6fb0fff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f51a038cdaf6cc1b1f1",
+    "url": "/gold_pokedex/static/js/main.33676b26.chunk.js"
+  },
+  {
+    "revision": "41f59fa021b88b53e8d1",
+    "url": "/gold_pokedex/static/js/runtime-main.a5d0921c.js"
+  },
+  {
+    "revision": "08ac77890f46a93418ebacc6dd1af1d2",
+    "url": "/gold_pokedex/static/media/pikachu_background.08ac7789.jpg"
+  }
+]);
